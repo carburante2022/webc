f0c9f94ba4964d1136c26a497318c0e0210cd349
user <user@example.com>
2022-09-23 11:46:50 +0200

    Update Github.cs

--- carburanti/Util/Scraper/Github.cs
+++ carburanti/Util/Scraper/Github.cs
@@ -12,7 +12,7 @@ namespace carburanti.Util.Scraper
         {
             string json = carburanti.Util.Downloader.Download("https://raw.githubusercontent.com/carburante2022/webc/main/data.json");
             
-            if (!json.Contains("<!DOCTYPE html>"))
+            if (!json.Contains("<!DOCTYPE html>") && string.IsNullOrEmpty(json) == false)
             {
                 var d = Newtonsoft.Json.JsonConvert.DeserializeObject<carburanti.Model.AllData>(json);
                 if (d != null)
