64e674f24df518073c3fd2bd53fda3ff18387517
user <user@example.com>
2022-09-23 14:29:45 +0200

    Update carburanti.sln.DotSettings.user and Program.cs

--- carburanti/Program.cs
+++ carburanti/Program.cs
@@ -7,7 +7,7 @@ carburanti.Util.Scraper.Github.Download();
 
 carburanti.Util.Scraper.CarburantiOpenDataScraper.Download();
 
-string s = Newtonsoft.Json.JsonConvert.SerializeObject(carburanti.VariabiliGlobali.VarGlob.allData);
+var s = Newtonsoft.Json.JsonConvert.SerializeObject(carburanti.VariabiliGlobali.VarGlob.allData);
 File.WriteAllText("data.json", s);
 
 carburanti.Util.Graph.Write();
