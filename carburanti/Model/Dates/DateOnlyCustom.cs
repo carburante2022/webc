e49273a6470801540a79629075eacf13300eb04c
user <user@example.com>
2022-09-23 12:08:18 +0200

    Update DateOnlyCustom.cs and DateTimeCustomTypeConverter.cs

--- carburanti/Model/Dates/DateOnlyCustom.cs
+++ carburanti/Model/Dates/DateOnlyCustom.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace carburanti.Model.Dates
 {
     [Serializable]
     [JsonObject(MemberSerialization.Fields)]
+    [TypeConverter(typeof(DateTimeCustomTypeConverter))]
     public class DateOnlyCustom
     {
         public int year;
@@ -22,8 +24,30 @@ namespace carburanti.Model.Dates
             this.day = dateOnly.Day;
         }
 
+        public DateOnlyCustom(string value)
+        {
+            var s = value.Split("-");
+            this.year = Convert.ToInt32(s[0]);
+            this.month = Convert.ToInt32(s[1]);
+            this.day = Convert.ToInt32(s[2]);
+        }
+
         public override string ToString() {
             return this.year.ToString() + "-" + this.month.ToString() + "-" + this.day.ToString();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null)
+                return false;
+
+            if (obj is DateOnlyCustom dt)
+            {
+                if (this.year == dt.year && this.month == dt.month && this.day == dt.day)
+                    return true;
+            }
+
+            return false;
         }
     }
 }
---
+++ carburanti/Model/Dates/DateTimeCustomTypeConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carburanti.Model.Dates
+{
+    internal class DateTimeCustomTypeConverter : TypeConverter
+    {
+ 
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            return sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            var casted = value as string;
+            return casted != null
+                ? new DateOnlyCustom(casted)
+                : base.ConvertFrom(context, culture, value);
+        }
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            var casted = value as DateOnlyCustom;
+            if (casted == null || destinationType == null)
+                return null;
+
+            var s = casted.ToString();
+            return destinationType == typeof(string)
+                ? s
+                : base.ConvertTo(context, culture, value, destinationType);
+        }
+    }
+}
