f9341d820ff89dcf73994a23ac6a21622cb89ab0
user <user@example.com>
2022-09-24 10:26:29 +0200

    Update Github.cs

--- carburanti/Util/Scraper/Github.cs
+++ carburanti/Util/Scraper/Github.cs
@@ -12,7 +12,7 @@ internal static class Github
 {
     internal static void Download()
     {
-        var json = Downloader.Download("https://raw.githubusercontent.com/carburante2022/webc/main/data.json");
+        var json = Downloader.Download("https://raw.githubusercontent.com/carburante2022/webc/main/data/data.json");
         if (json.Contains("<!DOCTYPE html>") || string.IsNullOrEmpty(json)) return;
         try
         {
